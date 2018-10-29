070e8c82fa6faa1f24757657e99d1a8430e20eda
Ziyi Chen <q363389749@gmail.com>
2018-10-28 20:42:47 -0400

    finish

---
+++ MyScreenSaver/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyScreenSaver
+{
+    public partial class frmScreenSaver : Form
+    {
+        List<Image> BGImages = new List<Image>();
+        List<BritPic> BritPics = new List<BritPic>();
+        Random rand = new Random();
+
+        class BritPic
+        {
+            public int PicNum;
+            public float x;
+            public float y;
+            public float Speed;
+        }
+
+        public frmScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void frmScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+
+        private void frmScreenSaver_Load(object sender, EventArgs e)
+        {
+            string[] images = System.IO.Directory.GetFiles("pics");
+
+            foreach(string image in images)
+            {
+                BGImages.Add(new Bitmap(image));
+            }
+            for(int i = 0; i < 50; ++i)
+            {
+                BritPic mp = new BritPic();
+                mp.PicNum = i % BGImages.Count;
+                mp.x = rand.Next(0, Width);
+                mp.y = rand.Next(0, Height);
+                BritPics.Add(mp);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void frmScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(BritPic bp in BritPics)
+            {
+                e.Graphics.DrawImage(BGImages[bp.PicNum], bp.x, bp.y);
+                bp.x -= 50;
+                if (bp.x < -250)
+                {
+                    bp.x = Width + rand.Next(20, 100);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Screen Saver
+Using c#(.NET Framework) to build.
+Press any key to stop the program.
+Screenshot:
+![alt text](https://github.com/OrangeYi/MyMusicPlayer/raw/master/MyMusicPlayer/screenshoot.png "INTERFACE")
+
+EXE download link:
+[Click me!](https://github.com/OrangeYi/MyMusicPlayer/raw/master/MyMusicPlayer/bin/Debug/MyMusicPlayer.exe)
+Tech point:
+1. Load images and drew them on the scerrn
+2. Creat custom classes to hold data
+3. Event handler
+4. Tinmer control
